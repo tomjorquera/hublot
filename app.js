6d5fbfdba82a56832e8c6792904634ecc5ece776
Tom Jorquera <tom.jorquera@linagora.com>
2017-09-14 16:11:31 +0200

    add some logs and avoid exiting too soon

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const config = require('./config.json');
 const runner = require('./lib/runner.js')(config.runner);
 const controller = require('./lib/controller.js')('./client');
 
+console.log('starting hublot...');
+
 controller.loadAll('controller', 'lib', 'robot')
   .then(modules => {
+    console.log('modules loaded... launching runner');
     // Note: result can be stored in a variable to control further the browser
     // e.g.: let client =  runner.run(...); client.end();
     runner.run(modules, config.visio.url, 'test-bot', config.client);
@@ -12,3 +15,8 @@ controller.loadAll('controller', 'lib', 'robot')
   .catch(err => {
     console.error(err);
   });
+
+// Hang
+(function wait() {
+  setTimeout(wait, 10000);
+})();
